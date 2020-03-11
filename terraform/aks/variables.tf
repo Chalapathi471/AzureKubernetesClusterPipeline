28d3e1201699392ea72472287cd0201cb0012d3b
Chalapathi471 <chalapathi471@gmail.com>
2020-03-11 19:45:09 +0530

    [Chalapathi - Location changed]

--- terraform/aks/variables.tf
+++ terraform/aks/variables.tf
@@ -7,23 +7,21 @@ variable environment {
 }
 
 variable location {
-    default = "westeurope"
+    default = "eastus"
 }
 
 variable node_count {
-  default = 3
+  default = 2
 }
-
-
 
 variable dns_prefix {
   default = "k8stest"
 }
 
 variable cluster_name {
-  default = "k8stest"
+  default = "aks-terraform-cluster"
 }
 
 variable resource_group {
-  default = "kubernetes"
+  default = "kubernetes-terraform-rg"
 }
